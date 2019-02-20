f892105971fc3485d384f53a14f59e8fb97cfac0
Rajeev Reddy <rajeevr69@yahoo.com>
2019-02-19 21:55:04 -0800

    Added DomContentLoaded

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-let delayTimer;
+document.addEventListener('DOMContentLoaded', ()=> {
 
-function timerFunction() {
-    delayTimer = setTimeout(showPage, 3000);//Calls the showPage function after 3 seconds upon index.html page finishes loading.
-}
+  let delayTimer;
 
-function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("front").style.opacity = "1";
-}
+  document.getElementById("timer").addEventListener("load", timerFunction());
+
+  function timerFunction() {
+        delayTimer = setTimeout(showPage, 3000);//Calls the showPage function after 3 seconds upon index.html page finishes loading.
+  }
+
+  function showPage() {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("front").style.opacity = "1";
+  }
+
+});
--- app1.js
+++ app1.js
@@ -1,9 +1,13 @@
-const today = new Date();
-const day = today.getDay();
-const  hour = today.getHours();
+document.addEventListener('DOMContentLoaded', ()=> {
 
-if (day == 1 && hour >= 9 && hour < 17) {
-      document.querySelector(".alertbox").style.display="flex";
-      } else {
-      document.querySelector(".alertbox").style.display="none";
-}
+  const today = new Date();
+  const day = today.getDay();
+  const  hour = today.getHours();
+
+  if (day == 1 && hour >= 9 && hour < 17) {
+        document.querySelector(".alertbox").style.display="flex";
+        } else {
+        document.querySelector(".alertbox").style.display="none";
+  }
+
+});
