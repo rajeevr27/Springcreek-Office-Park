bfdd7902d57650e045a3283b5a7d40d165c859e9
Rajeev Reddy <rajeevr69@yahoo.com>
2019-03-08 19:20:11 -0800

    Added document.readyState

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+function runFunction() {
 
-  let delayTimer;
+      let delayTimer;
 
-  document.getElementById("timer").addEventListener("load", timerFunction());
+      document.getElementById("timer").addEventListener("load", timerFunction());
 
-  function timerFunction() {
-        delayTimer = setTimeout(showPage, 3000);//Calls the showPage function after 3 seconds upon index.html page finishes loading.
-  }
+      function timerFunction() {
+            delayTimer = setTimeout(showPage, 3000);//Calls the showPage function after 3 seconds upon index.html page finishes loading.
+      }
 
-  function showPage() {
-        document.getElementById("loader").style.display = "none";
-        document.getElementById("front").style.opacity = "1";
-  }
+      function showPage() {
+            document.getElementById("loader").style.display = "none";
+            document.getElementById("front").style.opacity = "1";
+      }
 
-});
+}
+
+if (document.readyState === "loading") {
+      document.addEventListener("DOMContentLoaded", runFunction);
+    } else {
+      runFunction();
+}
