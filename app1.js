b11738429a4374b9331e0e8e134d5249209ad647
Rajeev Reddy <rajeevr69@yahoo.com>
2019-07-18 20:32:37 -0700

    Update app1.js

--- app1.js
+++ app1.js
@@ -4,16 +4,12 @@ function runFunction() {
 
       const today = new Date();
       let timeZone = today.getTimezoneOffset();
-      const springForward = new Date('March 10 2019 02:00:00');
-      const fallBack = new Date('November 3 2019 02:00:00');
       const day = today.getDay();
       const  hour = today.getHours();
 
-      if (day == 1 && hour >= 9 && hour < 15 && timeZone == 480 && today < springForward) {
+      if (day == 1 && hour >= 9 && hour < 15 && timeZone == 480) {
         document.querySelector(".alertbox").style.display="flex";
-      } else if (day == 1 && hour >= 9 && hour < 15 && timeZone == 420 && today >= springForward && today < fallBack){
-        document.querySelector(".alertbox").style.display="flex";
-      } else if (day == 1 && hour >= 9 && hour < 15 && timeZone == 480 && today >= fallBack){
+      } else if (day == 1 && hour >= 9 && hour < 15 && timeZone == 420){
         document.querySelector(".alertbox").style.display="flex";
       } else {
         document.querySelector(".alertbox").style.display="none";
